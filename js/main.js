bdc1471986b38e3b188ae130177bc882a1fadc7b
Медведев Борис <bmedved78@mail.ru>
2022-09-26 22:02:13 +0300

    filter

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const arrayStudents = [
 	new Student("Борис", "Медведев", "Николевич", new Date(1978, 9, 22), 2011, "Истории"),
 	new Student("Василий", "Кононов", "Петрович", new Date(1990, 5, 10), 2017, "Химии"),
 	new Student("Кирилл", "Медведев", "Борисович", new Date(2005, 6, 23), 2019, "Философии"),
+	new Student("Максим", "Зайцев", "Петрович", new Date(2011, 3, 27), 2020, "Математики"),
 ];
 
 const $studentsList = document.getElementById("students-list");
@@ -12,6 +13,7 @@ const $studentsListTHALL = document.querySelectorAll(".student__table th");
 
 // получить tr студента
 function newStudentTR(student) {
+
 	const $studentTR = document.createElement("tr"),
 		$fioTD = document.createElement("td"),
 		$facultyTD = document.createElement("td"),
@@ -21,7 +23,6 @@ function newStudentTR(student) {
 	$facultyTD.textContent = student.faculty;
 	$dateBirthTD.textContent = student.getAGE();
 	$startYearTD.textContent = student.getCourse();
-
 	$studentTR.append($fioTD);
 	$studentTR.append($facultyTD);
 	$studentTR.append($dateBirthTD);
@@ -41,13 +42,38 @@ document.getElementById('add-student').addEventListener('submit', function (even
 		Number(document.getElementById('input-startyear').value),
 		document.getElementById('input-faculty').value,
 	));
+
 	renderTable();
 });
+
+
+function filterTable(arr, prop, value) {
+	// let copy = [...arrayStudents];
+	arr = [...arrayStudents];
+
+	$studentsList.textContent = "";
+
+	for (const item of arr) {
+		if (String(item[prop]).includes(value) == true) {
+			arr.push(item);
+		}
+		return arr;
+	}
+
+}
+let arr2 = filterTable(arrayStudents, "getCourse()", "2011-2015");
+console.log(arr2);
+
+
+
+
+
+
+
 
 function renderTable() {
 	const arrayStudentsCopy = [...arrayStudents];
 	$studentsList.textContent = "";
-
 
 	for (const student of arrayStudentsCopy) {
 		$studentsList.append(newStudentTR(student));
